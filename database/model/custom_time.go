0aeb4844548d1fc58c47c4a97327fc5c1b1b98df
Avelino Bego <avelino.bego@gmail.com>
2023-02-10 17:45:32 -0300

    Agora extrai do time a string e pega data e hora

--- database/model/custom_time.go
+++ database/model/custom_time.go
@@ -1,29 +1,34 @@
 package model
 
-import "time"
+import (
+	"time"
+)
 
 type QualitecTime struct {
 	time.Time
 }
 
-func (c *QualitecTime) Scan(value interface{}) error {
+func (c *QualitecTime) Scan(value interface{}) (e error) {
+
 	switch t := value.(type) {
 	case string:
-		if v, e := time.Parse(time.RFC3339, t); e == nil {
-			c.Time = v
-		} else {
-			return e
-		}
+		c.Time, e = toDateTime(t)
 	case []uint8:
-		if v, e := time.Parse(time.RFC3339, string(t)); e == nil {
-			c.Time = v
-		} else {
-			return e
-		}
+		c.Time, e = toDateTime(string(t))
 	case time.Time:
 		c.Time = t
 	default:
-		c.Time = time.Now()
+		c.Time = time.Time{}
 	}
-	return nil
+	return
+}
+
+func toDateTime(v string) (result time.Time, err error) {
+	result = time.Time{}
+	var mapa map[string]string
+	if mapa, err = ExtractDateTime(v); err != nil {
+		return
+	}
+	result, err = time.Parse("2006-01-0215:04:05.999999999", mapa["date"]+mapa["time"])
+	return
 }
